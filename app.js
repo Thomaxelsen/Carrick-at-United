44d4c1c43f56cfe6cd6ad2285dac60069c21f894
Thomaxelsen <thomas.axelsen@gmail.com>
2026-02-10 23:31:24 +0100

    Disable caching when loading match data

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const renderStats = ({ kamper }) => {
   renderMatches(kamper);
 };
 
-fetch('data.json')
+fetch('data.json', { cache: 'no-store' })
   .then((response) => {
     if (!response.ok) {
       throw new Error('Could not load data.json');
